d95598bfd459a783ca21ec57e13b3a6caad7c9e2
Subhajit Sahu <wolfram77@gmail.com>
2019-12-08 18:57:12 +0530

    :mushroom: build.js can now write csv for PARTN.TXT

--- build.js
+++ build.js
@@ -1,12 +1,29 @@
 const fs = require('fs');
 
 
+var PARTS = 5;
+var FIELDS = ['code', 'name', 'synonyms', 'unit', 'tables', 'comments', 'examples'];
+var RENAME = new Map([
+  ['synonym', 'symonyms'],
+  ['table', 'tables'],
+  ['comment', 'comments'],
+  ['example', 'examples'],
+]);
+
+// excess spaces are trimmed from within (autophagy)
 function trimWithin(txt) {
   return txt.replace(/[\s\n]+/gm, ' ').replace(/\\n\s?/gm, '\n').trim();
 }
 
-function parseBlock(txt) {
-  var m = txt.match(/<(.+)>([\s\S]*?)\n((\s{5,8}|\t)\w+:[\s\S]*)/m);
+// comments start with #
+function removeComments(txt) {
+  return txt.replace(/^#[^\n]*\n/gm, '');
+}
+
+// see what a block looks like, in PARTN.TXT
+function readBlock(txt) {
+  var m = txt.match(/<(.+)>([\s\S]*?)\n((\s{4,8}|\t)\w+:[\s\S]*)/m);
+  if (m == null) console.log(txt);
   var blk = {code: m[1], name: trimWithin(m[2])}, rest = m[3];
   var ms = [...rest.matchAll(/\s{5,7}(\w+):/gm)];
   for (var i=0; i<ms.length; i++) {
@@ -14,16 +31,14 @@ function parseBlock(txt) {
     var bgn = ms[i].index + ms[i][0].length;
     var end = i+1 === ms.length? rest.length : ms[i+1].index;
     var val = rest.substring(bgn, end);
+    if (RENAME.has(key)) key = RENAME.get(key);
     blk[key] = trimWithin(val);
   }
   return blk;
 }
 
-function removeComments(txt) {
-  return txt.replace(/^#[^\n]*\n/gm, '');
-}
-
-function parseBlocks(txt) {
+// entire PARTN.TXT is read -> array of blocks
+function readBlocks(txt) {
   var blks = [];
   txt = removeComments(txt);
   var ms = [...txt.matchAll(/^<.+>|\n<\w+>/gm)];
@@ -31,14 +46,35 @@ function parseBlocks(txt) {
     var bgn = ms[i].index;
     var end = i+1 === ms.length? txt.length : ms[i+1].index;
     var blk = txt.substring(bgn, end);
-    blks.push(parseBlock(blk));
+    blks.push(readBlock(blk));
   }
   return blks;
 }
 
+// the generated csv looks like this:
+// field names
+// " quoted values ...
+function writeCsv(blks) {
+  var s = FIELDS.join()+'\n';
+  for (var blk of blks) {
+    for(var f of FIELDS) {
+      val = blk[f]||'';
+      val = val.replace(/\n/gm, '\\n');
+      s += `"${val}",`;
+    }
+    s = s.substring(0, s.length-1)+'\n';
+  }
+  return s;
+}
+
 
 function main() {
-  var part4 = fs.readFileSync('assets/PART4.TXT', 'utf8');
-  console.log(parseBlocks(part4));
+  for (var n=1; n<=PARTS; n++) {
+    console.log(`writing csv for part${n}`);
+    var txt = fs.readFileSync(`sources/PART${n}.TXT`, 'utf8');
+    var blks = readBlocks(txt);
+    var csv = writeCsv(blks);
+    fs.writeFileSync(`assets/part${n}.csv`, csv);
+  }
 }
 main();
